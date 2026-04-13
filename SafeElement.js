764af6119284b03a1e46e1a5feb123d0e3b9cb34
donpu333 <donpupi61@gmail.com>
2026-04-13 16:36:33 +0300

    Delete SafeElement.js

--- SafeElement.js
+++
@@ -1,8 +0,0 @@
-function safeElement(id) {
-    const el = document.getElementById(id);
-    if (!el) console.warn(`⚠️ Элемент "${id}" не найден в DOM`);
-    return el;
-}
-if (typeof window !== 'undefined') {
-    window.safeElement = safeElement;
-}
